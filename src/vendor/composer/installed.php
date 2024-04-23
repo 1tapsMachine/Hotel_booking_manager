3712b73a6ee8d7d5dbdaceda242c937c99b77f96
TOP G <samiztsouhailsami@gmail.com>
2024-04-23 10:55:07 +0100

    Update composer.json and composer.lock to include nesbot/carbon package

--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '96b9100bc498685e5d1bb4a697b0a2d06c349fb7',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2907437a0232e0379459d5c78fad635b2821c659',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -383,9 +383,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '96b9100bc498685e5d1bb4a697b0a2d06c349fb7',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2907437a0232e0379459d5c78fad635b2821c659',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -751,8 +751,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
