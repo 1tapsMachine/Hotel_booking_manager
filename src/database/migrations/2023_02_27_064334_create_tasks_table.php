bd115ff82bb64e43da7d8cc74f29e422433c25eb
TOP G <samiztsouhailsami@gmail.com>
2024-04-26 10:32:30 +0100

    Add progress column to tasks table in create_tasks_table migration

--- src/database/migrations/2023_02_27_064334_create_tasks_table.php
+++ src/database/migrations/2023_02_27_064334_create_tasks_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('date');
             $table->string('due_date');
             $table->integer('status');
+            $table->string('progress')->default(0);
             $table->timestamps();
         });
     }
